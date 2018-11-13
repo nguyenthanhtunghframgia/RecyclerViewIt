f932c3a97e0916df38fb35d333e63be046d85ac7
nguyenthanhtunghframgia <nguyen.thanh.tungh@framgia.com>
2018-11-13 14:30:56 +0700

    11122

--- app/src/main/java/com/example/framgianguyenthanhtungh/myapplication/Adapter.kt
+++ app/src/main/java/com/example/framgianguyenthanhtungh/myapplication/Adapter.kt
@@ -4,14 +4,15 @@ import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.example.framgianguyenthanhtungh.myapplication.databinding.ItemBinding
 
 class Adapter(val list: ArrayList<Item>, val onItemClick: (Item) -> Unit)
-    : RecyclerView.Adapter<BaseViewHolder<ViewDataBinding>>() {
+    : RecyclerView.Adapter<Adapter.Holder>() {
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): BaseViewHolder<ViewDataBinding> {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): Holder {
         val binding: ViewDataBinding = DataBindingUtil.inflate(
             LayoutInflater.from(p0.context),
             R.layout.item, p0, false
@@ -21,19 +22,22 @@ class Adapter(val list: ArrayList<Item>, val onItemClick: (Item) -> Unit)
                 if (this is ItemBinding) this.item?.let { onItemClick.invoke(it) }
             }
         }
-        return BaseViewHolder(binding)
+        return Holder(binding)
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    override fun onBindViewHolder(p0: BaseViewHolder<ViewDataBinding>, p1: Int) {
+    override fun onBindViewHolder(p0: Holder, p1: Int) {
         if (p0.binding is ItemBinding) p0.binding.item = list[p1]
         val textView: TextView = p0.binding.root.findViewById(R.id.text)
         textView.text = list[p1].string
         p0.binding.executePendingBindings()
     }
+
+
+    class Holder(val binding : ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
 
 //    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): Holder {
 //        val view = LayoutInflater.from(p0.getContext())
--- app/src/main/java/com/example/framgianguyenthanhtungh/myapplication/BaseViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.example.framgianguyenthanhtungh.myapplication
-
-import android.databinding.ViewDataBinding
-import android.support.v7.widget.RecyclerView
-
-class BaseViewHolder<out T : ViewDataBinding> constructor(val binding: T) : RecyclerView.ViewHolder(binding.root)
